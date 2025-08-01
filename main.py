ca667edf106a966b51d09b45e1f03d37752c99be
xujiiii <168079203+xujiiii@users.noreply.github.com>
2025-08-01 22:49:33 +0800

    just ok
    easy thing

---
+++ main.py
@@ -0,0 +1,3 @@
+import new as n
+
+print('decote')
--- new.py
+++ new.py
@@ -1,1 +1,3 @@
-print('hiiiiii, is it change to github?')
+def repeat(n:int):
+    for i in range(n):
+        print('hiiiiii, is it change to github?')
