c9ca121fd774977fb8e66d5aa837f98e7b7b155c
xujiiii <168079203+xujiiii@users.noreply.github.com>
2025-08-03 00:41:36 +0800

    do8-03

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 just to learn how to create github file
 
 new addition to the main
+
+continue to do merge 8-0-3
--- new.py
+++ new.py
@@ -1,3 +1,8 @@
 def repeat(n:int):
     for i in range(n):
         print('hiiiiii, is it change to github?')
+
+
+class fruit:
+    def __init__(self,name=None):
+        self.name=name
